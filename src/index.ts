dd5bebb064100c7d8c83256b1efbce985726ee4c
zhuyonghui <1129008920@qq.com>
2023-03-28 13:58:06 +0800

    新增导出组件

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { App } from "vue";
 import LText from "./components/LText";
-const components = [LText];
+import LShape from "./components/LShape";
+const components = [LText,LShape];
 
 const install = (app: App) => {
   components.forEach((com) => {
     app.component(com.name, com);
   });
 };
-export { LText, install };
+export { LText,LShape, install };
 export default { install };
